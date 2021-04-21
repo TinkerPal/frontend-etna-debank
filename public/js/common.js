355da0c32f9b1f296de888a853c9d57b4b3efb27
Iryna Vynnychenko <i.vynnychenko@gmail.com>
2021-04-21 15:38:12 +0300

    push changes

--- public/js/common.js
+++ public/js/common.js
@@ -197,23 +197,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
     pointerEvents: true
   });
 
-  var modals = document.querySelectorAll("[data-modal]");
-
-  modals.forEach(function (trigger) {
-    trigger.addEventListener("click", function (event) {
-      event.preventDefault();
-      var modal = document.getElementById(trigger.dataset.modal);
-      modal.classList.add("open");
-      var exits = modal.querySelectorAll(".modal-exit");
-      exits.forEach(function (exit) {
-        exit.addEventListener("click", function (event) {
-          event.preventDefault();
-          modal.classList.remove("open");
-        });
-      });
-    });
-  });
-
   var notifBlock = document.querySelector('.notif-block');
   var notifIcon = document.querySelector('.notif');
 
