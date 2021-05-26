0803393f3b6a8fd35ee02e045be472c12bce4896
Iryna Vynnychenko <i.vynnychenko@gmail.com>
2021-05-26 16:19:35 +0300

    added pointer none while page upload

--- src/js/utils.js
+++ src/js/utils.js
@@ -23,6 +23,7 @@ const isEmptyTable = (idContainer) => {
 const setLdBar = (value, part = 'null') => {
   const { ldBar } = document.getElementById('debank_load_bar');
   const ldBarWrapper = document.getElementById('load_bar_cover');
+  const body = document.querySelector('body');
 
   if (!value) {
     value = Number(ldBar.value) + Number(part);
@@ -35,9 +36,11 @@ const setLdBar = (value, part = 'null') => {
   if (ldBar.value === 100) {
     setTimeout(() => {
       ldBarWrapper.style.display = 'none';
+      body.classList.remove('page-loading');
     }, 1000);
   } else {
     ldBarWrapper.style.display = 'block';
+    body.classList.add('page-loading');
   }
 };
 
